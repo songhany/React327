707b4ab3d19184dea3fdfb168d68f08ffb05e4b1
Songhan Yu <songhanysh@gmail.com>
2023-04-06 21:09:18 -0400

    update latest

--- lecture/day8 mvc/todolist/api.js
+++ lecture/day8 mvc/todolist/api.js
@@ -1,42 +1,38 @@
-// Pros is only need to modify code in this file
-
 const API = (() => {
-  const API_URL = "http://localhost:3000/todos";
-  // reuturn a promise that will resolve to the todos
-
-  // return a promise that will resolve to the todos
-  // function getTodos() {
-  //   return fetch(API_URL).catch(res=>res.json());
-  // }
-
-  const getTodos = async() => {
-    const res = await fetch(API_URL);
-    return await res.json();
-  }
-
-  const postTodo = async(newTodo) => {
-    const res = 
-      await fetch(API_URL, {
+    const API_URL = "http://localhost:3000/todos";
+  
+    // return a promise that will resolve to the todos
+    // function getTodos() {
+    //       return fetch(API_URL).catch(res=>res.json());
+    // }
+  
+    const getTodos = async () => {
+      const res = await fetch(API_URL);
+      return await res.json();
+    };
+  
+    const postTodo = async (newTodo) => {
+      const postedTodo = await fetch(API_URL, {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(newTodo)
-      })
-    return await res.json();
-  }
-  // postTodos({ id: 4, title: "new Todo", completed: false})
+        body: JSON.stringify(newTodo),
+      });
+      return await postedTodo.json();
+    };
+  
 
-  const deleteTodo = async(id) => {
-    const res = await fetch(`${API_URL}/${id}`, {
-      method: "DELETE",
-    });
-    // return await res.json();
-  }
-
-  return {
-    getTodos,
-    postTodo,
-    deleteTodo,
-  };
-})();
+    const deleteTodo = async (id) => {
+      const deletedTodo = await fetch(`${API_URL}/${id}`, {
+        method: "DELETE",
+      });
+      // return await deletedTodo.json();
+    };
+  
+    return {
+      getTodos,
+      postTodo,
+      deleteTodo,
+    };
+  })();
--- lecture/day8 mvc/todolist/index.js
+++ lecture/day8 mvc/todolist/index.js
@@ -1,66 +1,184 @@
-class TodoView {
-  constructor() {
-    this.todoForm = document.querySelector(".todo-form");
-    this.todoInput = document.querySelector("#newTodo");
-    this.addBtn = document.querySelector("todo__actions--add");
-    this.DeleteBtn = document.querySelector("todo__action--delete");
-    this.todoList = document.querySelector(".todolist");
-  }
+class TodoView {  // View is responsible for rending data
+    constructor() {
+        this.todoForm = document.querySelector(".todo-form");
+        this.todoInput = document.querySelector("#newTodo");
+        this.addBtn = document.querySelector("todo__actions--add");
+        this.deleteBtn = document.querySelector("todo__action--delete");
+        this.todoList = document.querySelector(".todolist");
+    }
 
-  renderTodos(todos) {
-    // this.todoList.innerHTML = ``
-    node
-  }
+    createTodoElement(todo) {
+        const todoElem = document.createElement("div");
+        todoElem.classList.add("todo");
+        todoElem.setAttribute("todo-id", todo.id)
 
-  createTodoElement(todo) {
-    const todoElem = document.createElement("div");
-    todoElem.classList.add("todo");
-    const todoTitle = document.createElement("div");
-    todoTitle.in
-  }
+        const todoTitle = document.createElement("div");
+        todoTitle.classList.add("todo__title");
+        todoTitle.innerText = todo.title;
+
+        const todoActions = document.createElement("div");
+        todoActions.classList.add("todo__actions");
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.classList.add("todo__actions--delete");
+        deleteBtn.innerText = "Delete";
+        deleteBtn.setAttribute("remove-id", todo.id)
+
+        const editBtn = document.createElement("button");
+        editBtn.classList.add("todo__actions--edit");
+        editBtn.innerText = "Edit";
+
+        todoActions.appendChild(editBtn);
+        todoActions.appendChild(deleteBtn);
+        todoElem.appendChild(todoTitle);
+        todoElem.appendChild(todoActions);
+
+        return todoElem;
+    }
+    
+    renderTodos(todos) {
+        this.todoList.innerText = "";
+        todos.forEach((todo) => {
+          const todoElem = this.createTodoElement(todo);
+          this.todoList.appendChild(todoElem);
+        });
+
+        // todos.forEach((todo) => {
+        //   this.todoList.innerHTML += `
+        //     <div class="todo">
+        //         <div class="todo__title">${todo.title}</div>
+        //         <div class="todo__actions">
+        //             <button class="todo__action todo__action--edit">Edit</button>
+        //             <button class="todo__action todo__action--delete" remove-id=${todo.id}>Delete</button>
+        //         </div>
+        //     </div>
+        //     `;
+        // });
+    }
+
+    appendTodo(newTodo) {
+        const todoElem = this.createTodoElement(newTodo);
+        this.todoList.appendChild(todoElem);
+    }
+
+
+    removeTodoFromView(id){
+        const todoToRemove = document.querySelector(`[todo-id="${id}"]`);
+        todoToRemove.remove();
+    }
 }
 
+
 class TodoModel {
-  #todos;
-  constructor() {
-    this.#todos = [];
-    this.init();
-  }
+    #todos;
+    constructor() {
+        this.#todos = [];
+    }
+  
+    async fetchTodos() {
+        const todos = await API.getTodos();
+        this.todos = todos;
+        return todos;
+    }
+  
+    async addTodo(newTodo) {
+        const todo = await API.postTodo(newTodo);
+        this.#todos.push(todo);
+        return todo;
+    }
+  
+    async deleteTodoById(id){
+        await API.deleteTodo(id);
+    }
 
-  init() {
-    getTodos.then(todos => {
-      this.todos = todos;
-    })
-  }
+    // fetchTodos() {
+    //     return getTodos().then(todos => {
+    //         this.todos = todos;
+    //     })
+    // }
 
-  addTodo(newTodo) {
-    postTodo(newTodo).then(todo => {
-      this.#todos.push(todo);
-    });
-  }
+    // addTodo(newTodo) {
+    //     postTodo(newTodo).then(todo => {
+    //         this.#todos.push(todo);
+    //     });
+    // }
 
-  deleteTodo() {
-    this.todos.splice(index, 1);
-  }
+    // deleteTodo() {
+    //     this.todos.splice(index, 1);
+    // }
 
-  editTodo() {
-    this.todos[index] = todo;
-  }
+    // editTodo() {
+    //     this.todos[index] = todo;
+    // }
 
-  getTodos() {
-    return this.todos;
-  }
+    // getTodos() {
+    //     return this.todos;
+    // }
 }
 
 
 class TodoController {
-  constructor(model, view) {
-    this.model = model;
-    this.view = view;
-  }
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+        this.init();
+    }
+
+    init() {
+        this.model.fetchTodos().then(() => {
+            const todos = this.model.todos;  // get todo data from model
+            this.view.renderTodos(todos);    // render todo to View
+        });
+        this.setUpAddTodo();
+        this.setUpRemoveTodo();
+    }
+
+    setUpAddTodo() {
+        this.view.todoForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+            const todoTitle = this.view.todoInput.value;
+            this.model.addTodo(
+                { 
+                    title: todoTitle,
+                    completed: false
+                }
+            ).then((newTodo) => {
+                this.view.appendTodo(newTodo);
+            });
+        });
+    }
+
+    setUpRemoveTodo(){
+        // this.view.getAllTodos.forEach(todoElem=>{
+        //   todoElem.addEventListener('click', (e) => {
+        //     const target = e.target;
+        //     if(target.classList.contains('todo__actions--delete')){
+        //       const idToRemove = target.getAttribute('remove-id');
+        //       this.model.deleteTodoById(idToRemove).then(()=>{
+        //         this.view.removeTodoFromView(idToRemove)
+        //       })
+        //     }
+        //   })
+        // })
 
 
-
+        this.view.todoList.addEventListener('click', (e) => {
+            const target = e.target;
+            if(target.classList.contains('todo__actions--delete')) {
+                target.setAttribute("disabled", "true");
+                const idToRemove = target.getAttribute('remove-id');
+                
+                this.model
+                    .deleteTodoById(idToRemove)
+                    .then(()=>{
+                        this.view.removeTodoFromView(idToRemove)
+                    }).catch((err)=>{
+                        console.log(err);
+                        target.removeAttribute("disabled")
+                    })
+            }
+        })
+    }
 }
 
 const app = new TodoController(new TodoModel(), new TodoView());
