42e78d2f6009b7a5c0f142634f66d371bd526ba6
Songhan Yu <songhanysh@gmail.com>
2023-04-19 00:32:39 -0400

    implement jQuery API with class MyQuery

--- lecture/day8 jQuery/myQuery.js
+++ lecture/day8 jQuery/myQuery.js
@@ -32,10 +32,43 @@ class MyQuery {
     this.domElem.classList.add(className);
     return this;
   }
+
+
+  before(htmlString) {
+    const classNameMatchedElements = document.querySelectorAll("." + this.domElem.className);
+    classNameMatchedElements.forEach((element) => {
+      element.insertAdjacentHTML('beforebegin', htmlString);
+    });
+    return this;
+  }
+
+  after(htmlString) {
+    const classNameMatchedElements = document.querySelectorAll("." + this.domElem.className);
+    classNameMatchedElements.forEach((element) => {
+      element.insertAdjacentHTML('afterend', htmlString);
+    });
+    return this;
+  }
+
+  prepend(htmlString) {
+    const classNameMatchedElements = document.querySelectorAll("." + this.domElem.className);
+    classNameMatchedElements.forEach((element) => {
+      element.insertAdjacentHTML('afterbegin', htmlString);
+    });
+    return this;
+  }
+
+  append(htmlString) {
+    const classNameMatchedElements = document.querySelectorAll("." + this.domElem.className);
+    classNameMatchedElements.forEach((element) => {
+      element.insertAdjacentHTML('beforeend', htmlString);
+    });
+    return this;
+  }
 }
 
 const $$ = (query) => {
-  const domElem = document.querySelector(query);
+  // const domElem = document.querySelector(query);
 
   // const jQueryObj = {
   //   html: (innerHTML) => {
@@ -61,37 +94,51 @@ const $$ = (query) => {
   return new MyQuery(query);
 }
 
-$$.ajax = (requestObj) => {
-  const {url, data, success} = requestObj;
-  
-  if (data !== undefined) {
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) => res.json())
-      .then(success);
-  } else {
-    fetch(url)
-      .then((res) => res.json())
-      .then(success);
-  }
-}
 
-$$.ajax({
-  url: "https://jsonplaceholder.typicode.com/posts",
-  // data: {
-  //   userId: 1,
-  //   title: "Songhan Yu",
-  //   body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-  // },
-  success: (result) => {
-    console.log(result);
-  },
-});
+// $(".inner").before("<p>before</p>");
+$$(".inner").before("<p>before</p>");
+
+// $(".inner").after("<p>after</p>");
+$$(".inner").after("<p>after</p>");
+
+// $(".inner").prepend("<p>prepend</p>");
+$$(".inner").prepend("<p>prepend</p>");
+
+// $(".inner").append("<p>append</p>");
+$$(".inner").append("<p>append</p>");
+
+
+// $$.ajax = (requestObj) => {
+//   const {url, data, success} = requestObj;
+  
+//   if (data !== undefined) {
+//     fetch(url, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify(data),
+//     })
+//       .then((res) => res.json())
+//       .then(success);
+//   } else {
+//     fetch(url)
+//       .then((res) => res.json())
+//       .then(success);
+//   }
+// }
+
+// $$.ajax({
+//   url: "https://jsonplaceholder.typicode.com/posts",
+//   // data: {
+//   //   userId: 1,
+//   //   title: "Songhan Yu",
+//   //   body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+//   // },
+//   success: (result) => {
+//     console.log(result);
+//   },
+// });
 
 // $$( "button.continue ")
 //   .html("Next Step...")
@@ -121,6 +168,21 @@ $$.ajax({
 // });
 
 
-// Homework:
-// 1. Recreate Jquery on your own
-// 2. add three new methods from the API documentation
+
+  // Homework:
+  // 1. Recreate Jquery on your own
+  // 2. add three new methods from the API documentation
+  // https://stackoverflow.com/questions/14846506/append-prepend-after-and-before#:~:text=before()%20.-,.,The%20vice%2Dversa%20is%20for%20.
+  // 
+  // after(htmlString) {
+  //   const parser = new DOMParser();
+  //   const htmlElement = parser.parseFromString(htmlString, "text/html").body.firstChild;
+  //   const classNameMatchedElements = document.querySelectorAll("." + this.domElem.className);
+  //   classNameMatchedElements.forEach((element) => {
+  //     // console.log(element);
+  //     // console.log(element.parentNode);
+  //     // console.log(element.nextSibling);
+  //     element.parentNode.insertBefore(htmlElement.cloneNode(true), element.nextSibling);  // use parentNode.insertBefore to insert a cloned version of the new HTML element after it. The insertBefore method takes two arguments: the new element to insert, and the reference node (in this case, the next sibling of the current element). 
+  //   });
+  //   return this;
+  // }
