ba2ea6e17cba9e1b90dada586f2e8977d004b8c3
Songhan Yu <songhanysh@gmail.com>
2023-04-20 15:37:44 -0400

    update

--- lecture/day9/modular-system/commonjs/math.cjs
+++ lecture/day9/modular-system/commonjs/math.cjs
@@ -2,10 +2,7 @@ function sum(a,b){
   return a+b;
 }
 
-
-
 const pri = "private"
-
 
 module.exports = {
   sum
--- lecture/day9/modular-system/esm/index.mjs
+++ lecture/day9/modular-system/esm/index.mjs
@@ -1,7 +1,6 @@
 // es module
 // cannot use require
 
-
 import { sum } from "./math.mjs";
 
 console.log(sum(1, 2));
