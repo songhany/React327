da7bcf0b151bd8a8384982fa204581b3c2ad9664
Songhan Yu <songhanysh@gmail.com>
2023-04-26 13:32:29 -0400

    counter

---
+++ hw/counter/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ClassCounter from './components/ClassCounter';
+import FuncctionCounter from './components/FunctionCounter'
+
+function App() {
+  return (
+    <div>
+      <ClassCounter />
+      <FuncctionCounter />
+    </div>
+
+  );
+}
+
+export default App;
