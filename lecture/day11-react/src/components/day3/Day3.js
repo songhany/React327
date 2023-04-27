4762814ada22afce94de4ef9d4b604b5b31ab878
Songhan Yu <songhanysh@gmail.com>
2023-04-27 00:10:52 -0400

    make API request in react app

--- lecture/day11-react/src/components/day3/Day3.js
+++ lecture/day11-react/src/components/day3/Day3.js
@@ -3,12 +3,13 @@ import ParentChild from './ParentChild';
 import Unmount from './Unmount';
 import MultipleUpdates from './MultipleUpdates';
 import Immutable from './Immutable';
+import FetchAPI from './FetchAPI';
 
 export default class Day3 extends Component {  
   render() {
     return (
       <div>
-        <Immutable />
+        <FetchAPI />
       </div>
     )
   }
--- lecture/day11-react/src/components/day3/FetchAPI.js
+++ lecture/day11-react/src/components/day3/FetchAPI.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react'
 
+const API = "https://jsonplaceholder.typicode.com/posts";
+
+// after the component mounted, we are to going fetch the api
+// then update state based on api's data
+// write jsx, we are going to render the list
+
 export default class FetchAPI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+    this.handleAPICall = this.handleAPICall.bind(this);
+  }
+
+  // IMPORTANT: During the "Render phase", the component should Always be pure.  https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
+  // i.e. Up until you call the render method, the components should always be pure.
+  // Making a API call is a side effect. For all of those side effects, we only trigger them after the "render()" method is complete, so the place to make the API request is inside "componentDidMount()" method.
+  // componentDidMount() {
+  //   fetch(API)
+  //     .then(res => res.json())
+  //     .then((data) => {
+  //       this.setState({ posts: data });
+  //     });
+  // }
+
+  // instead of trigger fetch(API) call right after the component Mounted using above code, we can also choose to make a API call after the button is clicked.
+  handleAPICall() {
+    fetch(API)
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ posts: data });
+    });
+  }
+
   render() {
     return (
-      <div>FetchAPI</div>
-    )
+      <div>
+        <h1>Fetch API</h1>
+        <button onClick={this.handleAPICall}>Make API Call</button>
+        <div>
+          {this.state.posts.map((post) => {
+            return <div key={post.id}>
+              {post.title}
+            </div>;
+          })}
+        </div>
+      </div>
+    );
   }
 }
