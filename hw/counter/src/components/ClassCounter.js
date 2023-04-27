54d117a35340654018d0e24e4b589d2601cde685
Songhan Yu <songhanysh@gmail.com>
2023-04-26 21:01:19 -0400

    update

--- hw/counter/src/components/ClassCounter.js
+++ hw/counter/src/components/ClassCounter.js
@@ -5,20 +5,47 @@ export default class Counter extends Component {
     super(props);
     this.state = {
       count: 0
-    }
-    this.handleAdd = this.handleAdd.bind(this)
+    };
+    this.handleAdd = this.handleAdd.bind(this);
   }
 
   handleAdd() {
-     this.setState({count: this.state.count + 1})
+    // console.log("this", this);
+    this.setState({count: this.state.count + 1})
+    // console.log(this.state);
+  }
+
+  toggleTheme() {
+    this.setState({
+      theme: this.state.theme === "light" ? "dark" : "light"
+    });
+  }
+
+  componentDidMount() {
+    console.log("Commit phase:", "Component did mount");
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   // console.log("nextState", nextState);
+  //   // console.log("curState", this.state);
+  //   // if (nextState.count !== this.state.count) {
+  //   //   return true;
+  //   // }
+  //   console.log("determining if we should update");
+  //   return true;
+  // }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("component just updated");
   }
 
   render() {
+    console.log("Render phase:", "now being rendered");
     return (
       <div>
         <div>Counter: {this.state.count}</div>
         <button onClick={this.handleAdd}>add</button>
       </div>
-    )
+    );
   }
 }
