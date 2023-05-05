7913d9719cde088dbb958773bcfcd36125a8a590
Songhan Yu <songhanysh@gmail.com>
2023-05-04 21:21:04 -0400

    redux

--- lecture/redux/src/App.js
+++ lecture/redux/src/App.js
@@ -1,17 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import TodoListApp from './components/TodoListAppuseContext';
-import TodoListAppuseReducer from './components/TodoListAppuseReducer';
+import TodoListAppuseContext from './components/TodoListAppContext';
+import TodoListAppuseReducer from './components/TodoListAppReducer';
 import LearnReducer from './components/LearnReducer/LearnReducer';
 
 function App() {
   return (
     <div>
-      {/* <TodoListApp /> */}
-      {/* <LearnReducer /> */}
+      <A a="a">
+        <div>children element</div>
+      </A>
+      <TodoListAppuseContext />
+      <LearnReducer />
       <TodoListAppuseReducer />
     </div>
   )
+}
+
+function A({a, children}) {
+  console.log(children);
+  return <div>
+    {children}
+  </div>
 }
 
 export default App;
