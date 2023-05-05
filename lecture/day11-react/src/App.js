584833a3504f742de808d69ac7bd947194abbb37
Songhan Yu <songhanysh@gmail.com>
2023-05-05 17:47:05 -0400

    high order component

--- lecture/day11-react/src/App.js
+++ lecture/day11-react/src/App.js
@@ -5,7 +5,7 @@ import Day1 from './components/day1/Day1';
 import Day2 from './components/day2/Day2';
 import Day3 from './components/day3/Day3';
 import Day4 from './components/day4/Day4';
-// import Day5 from './components/day5/Day5';
+// import Day5 from './components/day5 hoc/Day5';
 import Day6 from './components/day6 custom-hook/Day6';
 
 
