f41f99b601d76d982c2b23060c470f3c53278fdc
Songhan Yu <songhanysh@gmail.com>
2023-05-11 13:50:17 -0400

    update

--- lecture/day11-react/src/App.js
+++ lecture/day11-react/src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Day1 from './components/day1/Day1';
-import Day2 from './components/day2/Day2';
-import Day3 from './components/day3/Day3';
-import Day4 from './components/day4/Day4';
+// import Day1 from './components/day1 react/Day1';
+// import Day2 from './components/day2 class-lifecycle/Day2';
+// import Day3 from './components/day3 class-componenet/Day3';
+// import Day4 from './components/day4 function-componenet/Day4';
 // import Day5 from './components/day5 hoc/Day5';
 import Day6 from './components/day6 custom-hook/Day6';
 
